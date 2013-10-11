a8ea713f6f5df1c19c0cc1f986018b018a2b346f
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2013-10-11 15:23:07 +0200

    Añadido algoritmo de cálculo de media de 3 números

--- 01-introduccion/01-funciones/elemental.erl
+++ 01-introduccion/01-funciones/elemental.erl
@@ -1,3 +1,15 @@
 -module(elemental).
 
 -compile([export_all]).
+
+%% 01 - Media de 3 números.
+%% -----------------------------------------------------------------------------
+%% Definir la función media3 tal que (media3 x y z) es la media aritmética de 
+%% los números x, y y z. Por ejemplo,
+%%
+%% media3 1 3 8 == 4.0
+%% media3 (-1) 0 7 == 2.0
+%% media3 (-3) 0 3 == 0.0
+
+media3(X, Y, Z) -> 
+	(X + Y + Z) / 3.
