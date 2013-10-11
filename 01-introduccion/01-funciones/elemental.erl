6ec48779b69409b0fa13124530dbef37b124850d
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2013-10-11 16:59:50 +0200

    Añadido algoritmo de suma de euros de una colección de monedas

--- 01-introduccion/01-funciones/elemental.erl
+++ 01-introduccion/01-funciones/elemental.erl
@@ -13,3 +13,16 @@
 
 media3(X, Y, Z) -> 
 	(X + Y + Z) / 3.
+
+%% 02 - Suma de euros de una colección de monedas.
+%% -----------------------------------------------------------------------------
+%% Definir la función sumaMonedas tal que (sumaMonedas a b c d e) es la suma de 
+%% los euros correspondientes a a monedas de 1 euro, b de 2 euros, c de 5 euros,
+%% d de 10 euros y e de 20 euros. Por ejemplo,
+%%
+%% sumaMonedas 0 0 0 0 1 == 20
+%% sumaMonedas 0 0 8 0 3 == 100
+%% sumaMonedas 1 1 1 1 1 == 38
+
+sumaMonedas(A, B, C, D, E) ->
+	1 * A + 2 * B + 5 * C + 10 * D + 20 * E.
