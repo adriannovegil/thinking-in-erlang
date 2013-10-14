be01b39770ca5c6b2a99659d9efea4083763e3d6
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2013-10-14 13:32:20 +0200

    Añadido algoritmo de suma de los cuadrados de los n primeros números

--- 01-introduccion/02-comprension/comprension.erl
+++ 01-introduccion/02-comprension/comprension.erl
@@ -1,3 +1,18 @@
 -module(comprension).
 
 -compile([export_all]).
+
+%% 01 - Suma de los cuadrados de los n primeros números.
+%% -----------------------------------------------------------------------------
+%% Definir, por comprensión, la función
+%%
+%% sumaDeCuadrados :: Integer -> Integer
+%%
+%% tal que (sumaDeCuadrados n) es la suma de los cuadrados de los primeros n 
+%% números; es decir, 1^2 + 2^2 + ... + n^2. Por ejemplo,
+%%
+%% sumaDeCuadrados 3 = 14
+%% sumaDeCuadrados 100 = 338350
+
+sumaDeCuadrados(N) ->
+	lists:sum([ math:pow(X, 2) || X <- lists:seq(1,N)]).
