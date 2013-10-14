f652ddb380c03df2ed3169b687ff1e464cb14c00
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2013-10-14 23:07:35 +0200

    Añadido algoritmo de replicación de un elemento

--- 01-introduccion/03-recursion/recursion.erl
+++ 01-introduccion/03-recursion/recursion.erl
@@ -14,3 +14,17 @@
 
 potencia(X, 1) -> X;
 potencia(X, N) ->  X * potencia(X, N - 1).
+
+%% 02 - Replicación de un elemento.
+%% -----------------------------------------------------------------------------
+%% Definir por recursión la función
+%% 
+%% replicate2 :: int -> a -> [a]
+%%
+%% tal que (replicate2 n x) es la lista formmada por n copias del elemento x. 
+%% Por ejemplo,
+%%
+%% replicate2 3 2 == [2,2,2]
+
+replicate2(1, X) -> [X];
+replicate2(N, X) -> [X] ++ replicate2(N - 1, X).
