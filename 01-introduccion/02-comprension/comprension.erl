331c8ed3af039ea506f9b0f1610504161b4c9a82
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2013-10-14 13:36:45 +0200

    Añadido algoritmo de aproximación del límite

--- 01-introduccion/02-comprension/comprension.erl
+++ 01-introduccion/02-comprension/comprension.erl
@@ -273,3 +273,42 @@ errorAproxE2Helper(X, Init, Fin) ->
 
 errorAproxE2(X) ->
 	errorAproxE2Helper(X, 1, 10).
+
+%% 09 - Aproximación del límite.
+%% -----------------------------------------------------------------------------
+
+%% 9.1 - Definir la función aproxLimSeno tal que (aproxLimSeno n) es la lista 
+%% cuyos elementos son los términos de la sucesión sen(1/m)/(1/m) desde 1 hasta 
+%% n. Por ejemplo,
+%% 
+%% aproxLimSeno 1 = 
+%% aproxLimSeno 2 = 
+
+aproxLimSeno(N) ->
+	[math:sin(1/M)/(1/M) || M <- lists:seq(1, N)].
+
+%% 9.2 - ¿Cuál es el límite de la sucesión sen(1/m)/(1/m)?
+
+%% Solución: El límite es 1.
+
+%% 9.3 - Definir la función errorLimSeno tal que (errorLimSeno x) es el menor 
+%% número de términos de la sucecsión sen(1/m)/(1/m) necesarios para obtener 
+%% su límite con un error menor que x. Por ejemplo,
+%% 
+%% errorLimSeno 0.1 == 2.0
+%% errorLimSeno 0.01 == 5.0
+%% errorLimSeno 0.001 == 13.0
+%% errorLimSeno 0.0001 == 41.0
+
+errorLimSenoHelper(X, Init, Fin) ->
+	Tmp = [M || M <- lists:seq(Init, Fin), 
+		abs(1 - math:sin(1/M)/(1/M)) < X],
+	if
+		length(Tmp) > 0 ->
+			lists:nth(1, Tmp);
+		true ->
+			errorLimSenoHelper(X, Fin + 1, Fin + 10)
+	end.
+
+errorLimSeno(X) ->
+	errorLimSenoHelper(X, 1, 10).
