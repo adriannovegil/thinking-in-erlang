66961476d4cbc1173355e180a9d4509d6846789c
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2013-10-14 13:37:54 +0200

    Añadido algoritmo de cálculo con ternas pitagóricas

--- 01-introduccion/02-comprension/comprension.erl
+++ 01-introduccion/02-comprension/comprension.erl
@@ -353,3 +353,63 @@ errorPiHelper(X, Init, Fin) ->
 
 errorPi(X) ->
 	errorPiHelper(X, 1, 10).
+
+%% 11 - Ternas pitagóricas.
+%% -----------------------------------------------------------------------------
+
+%% 11.1 - Una terna (x,y,z) de enteros positivos es pitagórica si x^2+Y^2 = z^2. 
+%% Usando una lista por comprensión, definir la función
+%%
+%% pitagoricas :: Int -> [(Int,Int,Int)]
+%%
+%% tal que (pitagoricas n) es la lista de todas las ternas pitagóricas cuyas 
+%% componentes están entre 1 y n. Por ejemplo,
+%%
+%% pitagoricas 10 == [(3,4,5), (4,3,5), (6,8,10), (8,6,10)]
+
+pitagoricas(N) ->
+	[[X, Y, Z] || X <- lists:seq(1, N), 
+				  Y <- lists:seq(1, N), 
+				  Z <- lists:seq(1, N),
+				  math:pow(X, 2) + math:pow(Y, 2) == math:pow(Z, 2)].
+
+%% 11.2 - Definir la función
+%%
+%% numeroDePares :: (Int, Int, Int) ->
+%%
+%% tal que (numeroDePares t) es el número de elementos pares de la terna t. Por
+%% ejemplo,
+%%
+%% numeroDePares (3,5,7) == 0
+%% numeroDePares (3,6,7) == 1
+%% numeroDePares (3,6,4) == 2
+%% numeroDePares (4,6,4) == 3
+
+numeroDePares([X, Y, Z]) ->
+	lists:sum([1 || N <- [X, Y, Z], N rem 2 == 0]).
+
+%% 11.3 - Definir la función
+%%
+%% conjetura :: int -> Bool
+%%
+%% tal que (conjetura n) se verifica si todas las ternas pitagóricas cuyas 
+%% componentes están entre 1 y n tienen un número impar de números pares. Por
+%% ejemplo,
+%%
+%% conjetura 10 == true
+
+conjetura(N) -> 
+	lists:foldl(fun(X, Y) -> X and Y end, true, 
+			    [numeroDePares(T) rem 2 /= 0 || T <- pitagoricas(N)]).
+
+%% 11.4 Demostrar la conjetura para todas las ternas pitagóricas.
+
+%% Solución: Sea (x,y,z) una terna pitagórica. Entonces x^2+Y^2 = z^2. Pueden
+%% darse 4 casos:
+%% Caso 1: x e y son pares. Entonces x^2, y^2 y z^2 también lo son. Luego el 
+%% número de componentes pares es 3 que es impar.
+%% Caso 2: x es par e y es impar. Entonces, x^2 es par, y^2 es impar y z^2 es
+%% impar. Luedo el número de componentes pares es 1 que es impar.
+%% Caso 3: x es impar e y es par. Análogo al caso 2.
+%% Caso 4: x e y son impares. Entonces x^2 e y^2 también son impares y z^2 es
+%% par. Luego el número de componentes pares es 1 impar.
