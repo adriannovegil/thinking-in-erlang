d8f0e1e4c344c88f846162c707769771db7e1048
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2013-10-14 23:07:12 +0200

    Añadido algoritmo de potencia de exponente natural

--- 01-introduccion/03-recursion/recursion.erl
+++ 01-introduccion/03-recursion/recursion.erl
@@ -1,3 +1,16 @@
 -module(recursion).
 
 -compile([export_all]).
+
+%% 01 - Potencia de exponente natural.
+%% -----------------------------------------------------------------------------
+%% Definir por recursión la función
+%%
+%% potencia :: int -> int -> int
+%%
+%% tal que (potencia x n) es x elevado al número natural n. Por ejemplo,
+%%
+%% potencia 2 3 == 8
+
+potencia(X, 1) -> X;
+potencia(X, N) ->  X * potencia(X, N - 1).
