299973813371265822ac35a7413ce77b53259400
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2013-10-14 12:46:51 +0200

    Añadido algoritmo de cálculo del área de un triándulo mediante la fórmula de Herón

--- 01-introduccion/01-funciones/elemental.erl
+++ 01-introduccion/01-funciones/elemental.erl
@@ -477,3 +477,17 @@ raices(A, B, C) ->
 		true -> 
 			"No tiene raices reales"
 	end.
+
+%% 29 - Área de un triángulo mediante la formula de Herón.
+%% -----------------------------------------------------------------------------
+%% En geometría, la formula de Herón, descubierta por Herón de Alejandría, dice 
+%% que el área de un triángulo cuyos lados miden a, b y c es 
+%% sqrt(s(s-a)(s-b)(s-c)), donde s es el semiperímetro s = (a+b+c)/2
+%% Definir la función area tal que (area a b c) es el área de un triángulo de 
+%% lados a, b y c. Por ejemplo,
+%%
+%% area 3 4 5 == 6.0
+
+area(A, B, C) ->
+	S = (A + B + C) / 2,
+	math:sqrt(S * (S - A) * (S - B) * (S - C)).
