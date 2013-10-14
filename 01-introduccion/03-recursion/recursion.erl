adf5abff2d45b50de92a7fd475352211fd523b73
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2013-10-14 23:07:57 +0200

    Añadido algoritmo de doble factorial

--- 01-introduccion/03-recursion/recursion.erl
+++ 01-introduccion/03-recursion/recursion.erl
@@ -28,3 +28,30 @@ potencia(X, N) ->  X * potencia(X, N - 1).
 
 replicate2(1, X) -> [X];
 replicate2(N, X) -> [X] ++ replicate2(N - 1, X).
+
+%% 03 - Doble factorial.
+%% -----------------------------------------------------------------------------
+%% El doble factorial de un número n se define por
+%%
+%% 0!! = 1
+%% 1!! = 1
+%% n!! = n*(n-2) * ... * 3 * 1, si n es impar
+%% n!! = n*(n-2) * ... * 4 * 2, si n es par
+%%
+%% Por ejemplo,
+%%
+%% 8!! = 8*6*4*2 = 384
+%% 9!! = 9*7*5*3*1 = 945
+%%
+%% Definir, por recursión, la función
+%%
+%% dobleFactorial :: Int -> Int
+%%
+%% tal que (dobleFactorial n) es el doble factorial de n. Por ejemplo,
+%%
+%% sobleFactorial 8 == 384
+%% sobleFactorial 9 == 945
+
+dobleFactorial(0) -> 1;
+dobleFactorial(1) -> 1;
+dobleFactorial(N) -> N * dobleFactorial(N - 2).
